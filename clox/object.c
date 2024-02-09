24564b2b11d6de6a36a8670b524c9689cebe6200
shinel94 <or_abel@naver.com>
2024-02-09 19:37:49 +0900

    closure 추가

--- clox/object.c
+++ clox/object.c
@@ -18,10 +18,27 @@ static Obj *allocateObject(size_t size, ObjType type)
     return object;
 }
 
+ObjClosure *newClosure(ObjFunction *function)
+{
+    ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue *, function->upvalueCount);
+
+    for (int i = 0; i < function->upvalueCount; i++)
+    {
+        upvalues[i] = NULL;
+    }
+
+    ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+    closure->function = function;
+    closure->upvalues = upvalues;
+    closure->upvalueCount = function->upvalueCount;
+    return closure;
+}
+
 ObjFunction *newFunction()
 {
     ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
     function->arity = 0;
+    function->upvalueCount = 0;
     function->name = NULL;
     initChunk(&function->chunk);
     return function;
@@ -81,6 +98,15 @@ ObjString *copyString(const char *chars, int length)
     return allocateString(heapChars, length, hash);
 }
 
+ObjUpvalue *newUpvalue(Value *slot)
+{
+    ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+    upvalue->location = slot;
+    upvalue->closed = NIL_VAL;
+    upvalue->next = NULL;
+    return upvalue;
+}
+
 static void printFunction(ObjFunction *function)
 {
     if (function->name == NULL)
@@ -95,6 +121,11 @@ void printObject(Value value)
 {
     switch (OBJ_TYPE(value))
     {
+    case OBJ_CLOSURE:
+    {
+        printFunction(AS_CLOSURE(value)->function);
+        break;
+    }
     case OBJ_FUNCTION:
         printFunction(AS_FUNCTION(value));
         break;
@@ -104,7 +135,9 @@ void printObject(Value value)
     case OBJ_STRING:
         printf("%s", AS_CSTRING(value));
         break;
-
+    case OBJ_UPVALUE:
+        printf("upvalue");
+        break;
     default:
         break;
     }
